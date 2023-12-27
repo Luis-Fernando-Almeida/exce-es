08c494a81b04ee69b810365247d71fd30c93fea0
Luis Fernando <luis.almeida.faria@gmail.com>
2023-12-27 19:54:32 -0300

    soluçlão ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,7 +1,6 @@
 package model.entities;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.SimpleTimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
@@ -38,9 +37,17 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkin, Date checkout){
+    public String updateDates(Date checkIn, Date checkout){
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)){
+            return "Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)){
+            return "Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkout;
+        return null;
     }
 
     @Override
