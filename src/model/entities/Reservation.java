710bc9fe6dbde77328bc378added379e5dc2cc6f
Luis Fernando <luis.almeida.faria@gmail.com>
2023-12-27 20:24:58 -0300

    Boa solução

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,4 +1,6 @@
 package model.entities;
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -10,11 +12,17 @@ public class Reservation {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {/* throws indica qeu um método pode lançar exceções especifícas.
+    O método não ira lidar diretamente com isso e sim repassar para quem chamar esse método. */
+        if (!checkOut.after(checkIn)){
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
-        this.checkIn = checkin;
-        this.checkOut = checkout;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
+
+
 
     public Integer getRoomNumber() {
         return roomNumber;
@@ -37,17 +45,16 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkout){
+    public void updateDates(Date checkIn, Date checkout) {
         Date now = new Date();
         if (checkIn.before(now) || checkOut.before(now)){
-            return "Reservation dates for update must be future dates";
+            throw new DomainException("Reservation dates for update must be future dates");
         }
         if (!checkOut.after(checkIn)){
-            return "Check-out date must be after check-in date";
+            throw new DomainException("Check-out date must be after check-in date");
         }
         this.checkIn = checkIn;
         this.checkOut = checkout;
-        return null;
     }
 
     @Override
